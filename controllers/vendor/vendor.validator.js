f3f962d22d7d18cad1dd202fa4239584d662b0a2
Hardik-Vanani <vananihardik8553@gmail.com>
2024-06-25 18:57:40 +0530

    second commit

--- controllers/vendor/vendor.validator.js
+++ controllers/vendor/vendor.validator.js
@@ -19,7 +19,6 @@ module.exports = {
 
     deleteVendor: validate({
         params: Joi.object({
-            
             id: Joi.string()
                 .pattern(/^[0-9a-fA-F]{24}$/)
                 .message("Invalid ObjectId")
